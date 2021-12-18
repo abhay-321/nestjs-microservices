ff1f85794ae35335a5a0859d821e9f032ca58b59
Abhay Ahire <abhayahire3@gmail.com>
2021-12-18 18:59:02 +0530

    feat : set up client for message pattern comm

--- micro-client/src/app.service.ts
+++ micro-client/src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { ClientProxyFactory, Transport, ClientProxy, ClientOptions } from '@nestjs/microservices';
+
+// Creating client options here, specifying address & port of server
+const clientOptions : ClientOptions = {
+  transport: Transport.TCP,
+  options: {
+    host: '127.0.0.1',
+    port: 3000,
+  },
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly client: ClientProxy;
+
+  constructor() {
+    this.client = ClientProxyFactory.create(clientOptions);
+  }
+
+  getUser(username) {
+    return this.client.send<User, string>('get_user', username);
   }
 }
