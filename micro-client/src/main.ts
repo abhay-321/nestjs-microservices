b728c4677e0ad46c4e35ceb531347eae99fc0666
Abhay Ahire <abhayahire3@gmail.com>
2021-12-18 18:59:02 +0530

    feat : set up client for message pattern comm

--- micro-client/src/main.ts
+++ micro-client/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Server listening at 3000, opening 3060 for client
+  await app.listen(3060);
 }
 bootstrap();
