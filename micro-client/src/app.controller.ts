df6c6b13ab41cf780c00cd40023fa1ffbdecbf09
Abhay Ahire <abhayahire3@gmail.com>
2021-12-26 13:10:11 +0530

    feat : set up kafka in client

--- micro-client/src/app.controller.ts
+++ micro-client/src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':id')
+  getHello(@Param('id') id) {
+    return this.appService.getUserById(id);
   }
 }
--- micro-client/src/app.service.ts
+++ micro-client/src/app.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Client, ClientKafka, Transport } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  
+  @Client({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'user',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'user-consumer' // consumer same as in micro service
+      }
+    }
+  })
+  client: ClientKafka;
+
+  async onModuleInit() {
+    /**
+     * Here We need to subscribe to topic,
+     * so that we get response back
+     */
+    this.client.subscribeToResponseOf('user-topic');
+    await this.client.connect();
+  }
+
+  getUserById(id : number) {
+    return this.client.send('user-topic', { userid : id }); ;
   }
 }
